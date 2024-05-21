e02d4c3405b350fe8fcfbdd8781df1dfdc86981f
SalsaNP777 <salsa.nputr777@gmail.com>
2024-05-21 12:56:28 +0700

    final

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] n = {12, 9, 13, 6, 10, 4, 7, 2};
+        var filteredArray = n.Where(x => x % 3 != 0).ToArray();
+
+        Array.Sort(filteredArray);
+
+        Console.Write("n = [ ");
+        foreach (var item in filteredArray)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine("]");
+    }
+}
